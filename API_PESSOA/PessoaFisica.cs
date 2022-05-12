f87a9ee6285df210eff90de6faa1aa4dda9ed1d3
Alexsander <alexsandergutierreez@gmail.com>
2022-05-11 22:35:31 -0300

    aula

---
+++ API_PESSOA/PessoaFisica.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_PESSOA
+{
+    class PessoaFisica : Pessoa
+    {
+        string CPF { get; set; }
+        public PessoaFisica(long Codigo, string Nome, string CPF) : base(Codigo, Nome) {
+            this.CPF = CPF;
+        }
+
+        public override string Imprimir()
+        {
+            string valor = String.Format("COD:{0}\tNome:{1}\tDOC:{2}", Codigo, Nome, CPF);
+            return valor;
+        }
+
+        public override bool Validar()
+        {
+            return false;
+        }
+    }
+}
